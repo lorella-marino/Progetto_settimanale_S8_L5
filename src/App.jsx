4d757e315495f929477814d974dd48609fe25d64
lorella-marino <lorellamarino@gmail.com>
2025-02-07 12:20:23 +0100

    commit 12:20

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavbar from "./components/MyNavbar";
 import MyFooter from "./components/MyFooter";
+import SectionTvShows from "./components/SectionTvShows";
 
 function App() {
   return (
     <div>
       <MyNavbar />
+      <SectionTvShows />
       <MyFooter />
     </div>
   );
---
+++ src/components/SectionTvShows.jsx
@@ -0,0 +1,34 @@
+import { Container, Dropdown, Button } from "react-bootstrap";
+import { BsTextLeft, BsGridFill } from "react-icons/bs";
+
+const SectionTvShows = () => {
+  return (
+    <section>
+      <Container id="tvshows" className="d-flex text-white align-items-center">
+        <h3>TV Shows</h3>
+
+        <Dropdown className="mx-4">
+          <Dropdown.Toggle variant="outline-light" className="rounded-1">
+            Genres
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="#">Horror</Dropdown.Item>
+            <Dropdown.Item href="#">Romantic</Dropdown.Item>
+            <Dropdown.Item href="#">Family</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <div className="d-flex ms-auto align-items-center">
+          <Button variant="outline-secondary" className="rounded-1 pt-0 pb-1 px-2">
+            <BsTextLeft />
+          </Button>
+          <Button variant="outline-secondary" className="rounded-1 pt-0 pb-1 px-2 ms-2">
+            <BsGridFill />
+          </Button>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default SectionTvShows;
